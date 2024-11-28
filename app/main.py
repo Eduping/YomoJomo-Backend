9c689cc38f7477ca4c39ce5a716815268dbf2380
GiHwan2 <sandwich5011@naver.com>
2024-11-29 08:31:30 +0900

    feat: cors middleware 설정

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from exceptions import http_exception_handler, validation_exception_handler, global_exception_handler, custom_exception_handler, CustomException
 from fastapi.exceptions import RequestValidationError
 from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from routes import user, chatroom, message, student_record
 app = FastAPI()
+
+# CORS 설정
+origins = [
+    "http://localhost:3000",
+    # "https://your-frontend-domain.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # 쿠키를 포함한 인증 정보를 허용하려면 True
+    allow_methods=["*"],  # 허용할 HTTP 메서드 (GET, POST 등)
+    allow_headers=["*"],  # 허용할 HTTP 헤더
+)
 
 app.add_exception_handler(HTTPException, http_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
